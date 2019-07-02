51a4c1615ee4530f2d5351ea3699c9443af4cb02
Emilija Mechkarova <emimeckarova@hotmail.com>
2019-07-03 00:31:14 +0200

    fix

--- PlaneSmash/PlaneSmash/InstructionsForm.Designer.cs
+++ PlaneSmash/PlaneSmash/InstructionsForm.Designer.cs
@@ -28,10 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstructionsForm));
             this.btnBack = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -45,9 +44,9 @@
             this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnBack.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnBack.Location = new System.Drawing.Point(146, 276);
+            this.btnBack.Location = new System.Drawing.Point(140, 217);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(168, 47);
+            this.btnBack.Size = new System.Drawing.Size(173, 66);
             this.btnBack.TabIndex = 0;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -55,40 +54,17 @@
             this.btnBack.MouseEnter += new System.EventHandler(this.btnBack_MouseEnter);
             this.btnBack.MouseLeave += new System.EventHandler(this.btnBack_MouseLeave);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Location = new System.Drawing.Point(22, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 46);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Use";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.DarkRed;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.Location = new System.Drawing.Point(298, 53);
+            this.label2.Location = new System.Drawing.Point(75, 127);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 46);
+            this.label2.Size = new System.Drawing.Size(117, 46);
             this.label2.TabIndex = 3;
-            this.label2.Text = "to Move";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.DarkRed;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label3.Location = new System.Drawing.Point(25, 163);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 46);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "and";
+            this.label2.Text = "Move";
             // 
             // label4
             // 
@@ -96,28 +72,27 @@
             this.label4.BackColor = System.Drawing.Color.DarkRed;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label4.Location = new System.Drawing.Point(269, 163);
+            this.label4.Location = new System.Drawing.Point(270, 127);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(171, 46);
+            this.label4.Size = new System.Drawing.Size(126, 46);
             this.label4.TabIndex = 5;
-            this.label4.Text = "to Shoot";
+            this.label4.Text = "Shoot";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::PlaneSmash.Properties.Resources.z;
-            this.pictureBox2.Location = new System.Drawing.Point(129, 130);
+            this.pictureBox2.Location = new System.Drawing.Point(261, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(115, 90);
+            this.pictureBox2.Size = new System.Drawing.Size(135, 112);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::PlaneSmash.Properties.Resources.fin;
-            this.pictureBox1.Location = new System.Drawing.Point(113, 35);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(176, 78);
+            this.pictureBox1.Size = new System.Drawing.Size(195, 112);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -127,13 +102,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
-            this.ClientSize = new System.Drawing.Size(472, 335);
+            this.ClientSize = new System.Drawing.Size(447, 312);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBack);
             this.Name = "InstructionsForm";
             this.Text = "InstructionsForm";
@@ -147,10 +120,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox2;
     }
